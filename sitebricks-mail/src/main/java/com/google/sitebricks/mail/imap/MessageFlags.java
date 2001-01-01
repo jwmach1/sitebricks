8c02aee39f84fb291c351bd4eccedf97d11368c5
Jochen Bekmann <jochen@pedesis.org>
2001-01-01 11:17:56 +1100

    adding STORE support

---
+++ sitebricks-mail/src/main/java/com/google/sitebricks/mail/imap/MessageFlags.java
@@ -0,0 +1,20 @@
+//package com.google.sitebricks.mail.imap;
+//
+//import java.util.EnumSet;
+//import java.util.Iterator;
+//
+//public class MessageFlags {
+//
+//  private EnumSet<Flag> flags = EnumSet.noneOf(Flag.class);
+//
+//  public MessageFlags() {
+//  }
+//
+//  public MessageFlags(EnumSet<Flag> flags) {
+//    this.flags = flags;
+//  }
+//
+//  public EnumSet<Flag> getFlags() {
+//    return flags;
+//  }
+//}
